6bddde8bc401da07fb7bb4b2972b4ff847ce024a
Berken Tekin <tekin.berken01@gmail.com>
2022-01-14 11:11:07 +0100

    Code style

--- public/javascript/client/game.js
+++ public/javascript/client/game.js
@@ -62,42 +62,42 @@ function connect()
         let piece;
         switch (message.type)
         {
-        case TPLAYERT:
-            player_type = message.data;
-            break;
-        case TGAMESTART:
+            case TPLAYERT:
+                player_type = message.data;
+                break;
+            case TGAMESTART:
                 if (player_type === "black") {
                     var whiteTimer = document.getElementById("timer-white");
                     var blackTimer = document.getElementById("timer-black");
                     whiteTimer.id = "timer-black";
                     blackTimer.id = "timer-white";
                     setBlackBoard();
-                     
+                    
                 }
-            clearInterval(waitingInterval);
-            gameStarted = true;
-            let textColour = player_type === "white" ? "var(--light-theme)": "black";
-            gameStatus.style.color = "black";
-            gameStatus.innerHTML = "Your are player: " + `<span style="color:${textColour};">${player_type}</span>`; 
-            break;
-        case TRESPONSE:
-            console.log("Server: ", message.data);
-            break;
-        case TTURN:
-            // Maybe highlight timer to make it obvious
-            break;
-        case TTABLE:
-            moveBox.writeMove(message.data["turn"], message.data["move"]);
-            break;
-        case TUPDATE:
-            piece = message.data["piece"];    
+                clearInterval(waitingInterval);
+                gameStarted = true;
+                let textColour = player_type === "white" ? "var(--light-theme)": "black";
+                gameStatus.style.color = "black";
+                gameStatus.innerHTML = "Your are player: " + `<span style="color:${textColour};">${player_type}</span>`; 
+                break;
+            case TRESPONSE:
+                console.log("Server: ", message.data);
+                break;
+            case TTURN:
+                // Maybe highlight timer to make it obvious
+                break;
+            case TTABLE:
+                moveBox.writeMove(message.data["turn"], message.data["move"]);
+                break;
+            case TUPDATE:
+                piece = message.data["piece"];    
                 finalizeMove(getPiece(getSquare(piece["pos"])), getSquare(encodePos(message.data["pieceTo"])));
                 let flag = message.data["moveInfo"]["flags"];
                 if (flag === 'k' || flag === 'q') {
                     castle(flag, message.data["moveInfo"]["color"]);
                 }
                 else if (flag === 'e') {
-                    enPassant(flag, message.data["moveInfo"]["color"], message.data["pieceTo"]);
+                    enPassant(message.data["moveInfo"]["color"], message.data["pieceTo"]);
                 }
                 var remainingSeconds = message.data["time"];
                 var minutes = remainingSeconds / 60 | 0; // Get the integer part
@@ -107,22 +107,20 @@ function connect()
                 var displayTimer = document.getElementById(`timer-${message.data["color"]}`);
                 displayTimer.innerText = `${minutes}:${seconds}`;
                 break;
-
             case TINVALID:  // Player has commited a nono
                 piece = message.data["piece"];
                 invalidMove(getPiece(getSquare(piece["pos"])));
                 break;
-
             case TWON:
                 game_over("You finished a match...");
                 break;
             case TTIME:
-                    var remainingSeconds = message.data["time"];
-                    var minutes = remainingSeconds / 60 | 0; // Get the integer part
-                    var seconds = remainingSeconds % 60;
-                    if (seconds < 10) seconds = `0${seconds}`;
-                    var displayTimer = document.getElementById(`timer-${message.data["color"]}`);
-                    displayTimer.innerText = `${minutes}:${seconds}`;
+                var remainingSeconds = message.data["time"];
+                var minutes = remainingSeconds / 60 | 0; // Get the integer part
+                var seconds = remainingSeconds % 60;
+                if (seconds < 10) seconds = `0${seconds}`;
+                var displayTimer = document.getElementById(`timer-${message.data["color"]}`);
+                displayTimer.innerText = `${minutes}:${seconds}`;
                 break;
             case TCHAT:
                 addEntry(message.data);
